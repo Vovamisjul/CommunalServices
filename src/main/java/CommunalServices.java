2e70e4f90570316258097d64376a19a10ef436f6
vovamisjul <vovamisjul@gmail.com>
2019-09-30 12:58:22 +0300

    create communalService javabean

--- src/main/java/CommunalServices.java
+++ src/main/java/CommunalServices.java
@@ -4,5 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommunalServices {
-    private List<House> servisedHouses = new ArrayList<>();
+    private List<House> servicedHouses = new ArrayList<>();
+
+    public CommunalServices() {
+    }
+
+    public void addHouse(House house) {
+        servicedHouses.add(house);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (var house: servicedHouses
+             ) {
+            result.append(house).append("\n");
+        }
+        return result.substring(0, result.length() - "\n".length());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != getClass())
+            return false;
+        var service = (CommunalServices)obj;
+        return service.servicedHouses.equals(servicedHouses);
+    }
 }
--- src/main/java/Entities/House.java
+++ src/main/java/Entities/House.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class House {
+    private HouseAddress address = new HouseAddress();
+    private List<Apartment> apartments = new ArrayList<>();
+
     public HouseAddress getAddress() {
         return address;
     }
 
-    private HouseAddress address = new HouseAddress();
-
     public List<Apartment> getApartments() {
         return apartments;
     }
-
-    private List<Apartment> apartments = new ArrayList<>();
 
     public House() {
     }
@@ -32,5 +31,32 @@ public class House {
         if (i > apartments.size() || i < 0)
             throw new IllegalArgumentException("Number of apartment is out of bounds!");
         return apartments.get(i);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Address: ").append(address).append("\n").append("Apartments: ");
+        for (var house: apartments
+        ) {
+            result.append(house).append("\n");
+        }
+        return result.substring(0, result.length() - "\n".length());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != getClass())
+            return false;
+        var house = (House)obj;
+        return house.apartments.equals(apartments) &&
+                house.address.equals(address);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
